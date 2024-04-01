b07343558214ab4e65796bc83732d25352d330af
Vladimir <volodya.kochetov.00@bk.ru>
2024-04-01 15:44:06 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void ReversePrint(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            ReversePrint(arr, index - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        ReversePrint(arr, arr.Length - 1);
+
+        Console.ReadLine();
+    }
+}
