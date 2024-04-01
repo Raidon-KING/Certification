51279856db7e154bab26a964efb6e8a9b782f4ef
Vladimir <volodya.kochetov.00@bk.ru>
2024-04-01 15:31:13 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int m = 1;
+int n = 5;
+
+Console.Clear();
+PrintNaturalNumbers(m, n);
+
+void PrintNaturalNumbers(int m, int n)
+{
+    if (m > n)
+        return;
+    Console.WriteLine(m);
+    PrintNaturalNumbers(m + 1, n);
+}
